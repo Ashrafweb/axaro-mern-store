19fdc6a9602c4473176a7bf444c761a418e8f470
Ashrafweb <76968589+Ashrafweb@users.noreply.github.com>
2025-01-24 12:13:16 +0600

    updated base url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-
+import cors from "cors"
 dotenv.config();
 const port = process.env.PORT || 3000;
 
@@ -19,6 +19,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors({ methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], origin: "*", credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

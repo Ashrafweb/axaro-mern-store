3594a420f30697546a953f97ad75dd7fab0dc4cf
Ashrafweb <76968589+Ashrafweb@users.noreply.github.com>
2025-01-24 19:28:07 +0600

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_NODE_ENV == "development" ? "http://localhost:5000" : "https://axaro-backend.vercel.app";
+export const BASE_URL = import.meta.env.VITE_NODE_ENV == "development" ? "http://localhost:5000" : "https://axaro-mern-backend.vercel.app";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";

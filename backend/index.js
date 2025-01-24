679fce7b9f1e5f596319879a1b3c9a5cb3de5c29
Ashrafweb <76968589+Ashrafweb@users.noreply.github.com>
2025-01-24 18:08:24 +0600

    .

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import cors from "cors"
-import { fetchProducts } from "./controllers/productController.js";
+
 dotenv.config();
 const port = process.env.PORT || 3000;
 
@@ -20,17 +19,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], origin: "*", credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.send("server running")
-})
-app.get("/products", (req, res) => {
-  const data = fetchProducts()
-  res.send(data)
-})
+
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
@@ -43,4 +35,6 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+app.use("/", express.static(path.join(__dirname + "/dist")));
+
 app.listen(port, () => console.log(`Server running on port: ${port}`));
